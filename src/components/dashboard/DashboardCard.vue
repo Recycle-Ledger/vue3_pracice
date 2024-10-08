d8db10339e85883af9ecc895d8a82b7f7d647086
dominic9636 <dominic@recycleledger.com>
2024-10-08 11:27:00 +0900

    [기능] 대시보드 summary데이터 패칭 및 서머리 데이터 카드 완성

--- src/components/dashboard/DashboardCard.vue
+++ src/components/dashboard/DashboardCard.vue
@@ -1,24 +1,98 @@
 <template>
-    <div class="h-24 bg-white rounded-md py-2 px-4 flex gap-3">
-        <img src="/assets/images/logomark_big.png" alt="logomark_big" class="w-2/12">
-        <div class="flex flex-col justify-evenly">
-            <p class="text-opacity-50 text-black">대시보드</p>
-            <p>1502kg</p>
-        </div>
-        <div class="flex flex-col justify-evenly">
-            <p class="text-opacity-50 text-black">대시보드</p>
-            <p>1502kg</p>
-        </div>
-        <div class="flex flex-col justify-evenly">
-            <p class="text-opacity-50 text-black">대시보드</p>
-            <p>1502kg</p>
-        </div>
+  <div
+    class="h-24 bg-white rounded-md py-2 px-4 flex gap-5 w-full justify-center items-center"
+  >
+    <div
+      class="flex-shrink-0 p-4 rounded-md bg-opacity-20"
+      :class="[
+        bgColor === 'red' ? 'bg-red-500' : '',
+        bgColor === 'blue' ? 'bg-blue-500' : '',
+        bgColor === 'yellow' ? 'bg-yellow-500' : '',
+      ]"
+    >
+      <component
+        :is="iconMap[icon]"
+        class="w-8"
+        :class="[
+          textColor === 'red' ? 'text-red-500' : '',
+          textColor === 'blue' ? 'text-blue-500' : '',
+          textColor === 'yellow' ? 'text-yellow-500' : '',
+        ]"
+      />
     </div>
+
+    <div class="flex flex-col w-full">
+      <div v-if="typeof value === 'number'">
+        <p class="text-opacity-50 text-black">{{ title }}</p>
+        <p class="text-2xl font-bold">
+          {{ cardValue }}
+        </p>
+      </div>
+
+      <div v-else class="flex w-full justify-start gap-10">
+        <div class="flex flex-col">
+          <p class="text-opacity-50 text-black line-clamp-2">인증 진행</p>
+          <p class="text-2xl font-bold">{{ value.inProgress }}</p>
+        </div>
+        <div class="flex flex-col">
+          <p class="text-opacity-50 text-black line-clamp-2">인증 승인</p>
+          <p class="text-2xl font-bold">{{ value.approved }}</p>
+        </div>
+        <div class="flex flex-col">
+          <p class="text-opacity-50 text-black line-clamp-2">인증 거부</p>
+          <p class="text-2xl font-bold">{{ value.reject }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { computed, PropType } from "vue";
+import { CertificationData } from "../../service/dashboard/summaryService";
+import { formatNumber } from "../../service/numberFormat";
+import {
+  CurrencyDollarIcon,
+  HomeModernIcon,
+  CheckBadgeIcon,
+} from "@heroicons/vue/24/outline";
+
+const cardValue = computed<string | CertificationData>(() => {
+  if (typeof props.value === "number") {
+    return formatNumber(props.value) + "kg";
+  }
+  return props.value;
+});
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: [Number, Object] as PropType<number | CertificationData>,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+  bgColor: {
+    type: String,
+    required: true,
+  },
+  textColor: {
+    type: String,
+    required: true,
+  },
+});
+
+// Heroicon 컴포넌트 매핑 => 매핑을 해야 import를 인식함
+const iconMap: Record<string, any> = {
+  CurrencyDollarIcon: CurrencyDollarIcon,
+  HomeModernIcon: HomeModernIcon,
+  CheckBadgeIcon: CheckBadgeIcon,
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="flex flex-col gap-5 mx-auto">
     <div class="grid lg:grid-cols-3 md:grid-cols-1 gap-5">
-      <DashboardCard />
-      <DashboardCard />
-      <DashboardCard />
+      <DashboardCard
+        title="총 구매 물량"
+        :value="summaryData.totalPurchased"
+        icon="CurrencyDollarIcon"
+        bgColor="yellow"
+        textColor="yellow"
+      />
+      <DashboardCard
+        title="현재 재고 물량"
+        :value="summaryData.totalStock"
+        icon="HomeModernIcon"
+        bgColor="red"
+        textColor="red"
+      />
+      <DashboardCard
+        title="인증 진행"
+        :value="summaryData.certification"
+        icon="CheckBadgeIcon"
+        bgColor="blue"
+        textColor="blue"
+      />
     </div>
     <div class="grid lg:grid-cols-3 md:grid-cols-1 gap-5">
       <BarChart />
@@ -22,6 +40,35 @@ import LineChart from "../components/dashboard/LineChart.vue";
 import BarChart from "../components/dashboard/BarChart.vue";
 import DoughnutChart from "../components/dashboard/DoughnutChart.vue";
 import DataTable from "../components/table/DataTable.vue";
+import { getSummary, SummaryData } from "../service/dashboard/summaryService";
+import { onMounted, ref } from "vue";
+import router from "../router";
+import { logout } from "../service/loginService";
+
+const summaryData = ref<SummaryData>({
+  totalStock: 0,
+  totalPurchased: 0,
+  certification: {
+    inProgress: 0,
+    approved: 0,
+    reject: 0,
+  },
+});
+
+onMounted(async () => {
+  const token: string = sessionStorage.getItem("recycle-token") || "";
+  if (token === "") {
+    await logout();
+    router.replace("/login");
+  }
+  const data: SummaryData | null = await getSummary(token);
+  if (!data) {
+    await logout();
+    console.error(data);
+    router.replace("/login");
+  }
+  summaryData.value = data!;
+});
 </script>
 
 <style scoped></style>
